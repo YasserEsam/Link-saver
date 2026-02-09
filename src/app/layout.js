f57cd29f0d63cea36b49f732dd17b5083848b654
YasserEsam <yaaser10esam1010@gmail.com>
2026-02-09 18:02:24 +0300

    feat: add chat widget

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import { Inter, Cairo } from "next/font/google";
 import "./globals.css";
 import Providers from "../components/Providers";
+import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"] });
 const cairo = Cairo({ subsets: ["arabic", "latin"] });
@@ -17,6 +18,17 @@ export default function RootLayout({ children }) {
         <Providers>
           {children}
         </Providers>
+        <Script
+          src="https://velents-shared.s3.us-east-1.amazonaws.com/loader.global.js"
+          data-agent-id="1"
+          data-auth-token="null"
+          data-plugin-mode="true"
+          data-widget="https://velents-shared.s3.us-east-1.amazonaws.com/widget.html"
+          data-logo="https://velents-shared.s3.us-east-1.amazonaws.com/Logo-Icon.svg"
+          data-color="#f7dc6f"
+          data-brand="Velents"
+          strategy="lazyOnload"
+        />
       </body>
     </html>
   );
